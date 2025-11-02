5f2d27bb6adca0bd2aee340618faa9be49a47c46
Taisuke Fujise <tafujise@c3r1s1.42tokyo.jp>
2025-11-02 23:31:05 +0900

    feat: ヘッダファイルの編集（ソート関数はコメントアウト中）

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 22:10:31 by tafujise          #+#    #+#             */
-/*   Updated: 2025/11/02 08:18:36 by tafujise         ###   ########.fr       */
+/*   Updated: 2025/11/02 23:24:36 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # define FAILURE -1
 
 # include "../libft/libft.h"
+# include <stdio.h>
 
 typedef struct s_list
 {
@@ -25,13 +26,42 @@ typedef struct s_list
 	struct s_list	*prev;
 }	t_list;
 
-typedef struct s_stacks
-{
-	t_list	stack_a;
-	t_list	stack_b;
-}	t_stacks;
+// typedef struct s_stacks
+// {
+// 	t_list	stack_a;
+// 	t_list	stack_b;
+// }	t_stacks;
 
-int	parse_args(int argc, char **argv, t_list **stack_a);
+// print_error.c
+void	print_error(void);
+// parse_args.c
+int		parse_args(int argc, char **argv, t_list **stack_a);
+//check_error.c
+int		check_error(char **num_char_set);
+int		check_digit(char **num_char_set);
+int		check_int_limits(char **num_char_set);
+int		check_duplicates(char **num_char_set);
+// limits.c
+int		is_in_int_min(char *num_str);
+int		is_in_int_max(char *num_str);
+int		is_in_int_limits(char *num_str);
+// free.c
+void	free_num_char_set(char **num_char_set);
+void	free_stack(t_list **stack);
 
+// utils.c
+int		ft_strcmp(const char *s1, const char *s2);
+int		count_set_size(char **num_char_set);
+int		*ft_atoi_map(char **num_char_set);
+// list.c
+t_list	*ft_lstnew(int	content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+// init.c
+int	init_stack(char **num_char_set, t_list **stack_a);
+// sort.c
+//void	do_sort(t_list **stack_a, t_list **stack_b)
 
 #endif
