de511baaed84548fed707fdfa818a226b694f580
Taisuke Fujise <tafujise@c3r1s1.42tokyo.jp>
2025-11-05 23:51:15 +0900

    fix : libftの修正 	- ft_strjoinのNULLガードの追加

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 09:18:11 by tafujise          #+#    #+#             */
-/*   Updated: 2025/10/17 22:07:59 by tafujise         ###   ########.fr       */
+/*   Updated: 2025/11/05 23:49:17 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strdup(const char *s)
 	char	*dst;
 	size_t	i;
 
+	if (!s)
+		return (NULL);
 	src = (char *)s;
 	i = 0;
 	dst = malloc(sizeof(char) * (ft_strlen(src) + 1));
--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tafujise <tafujise@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 10:09:58 by tafujise          #+#    #+#             */
-/*   Updated: 2025/10/17 22:08:06 by tafujise         ###   ########.fr       */
+/*   Updated: 2025/11/05 23:49:02 by tafujise         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,21 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*res;
 	size_t	total_len;
 
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	total_len = ft_strlen(s1) + ft_strlen(s2);
 	res = malloc(sizeof(char) * (total_len + 1));
 	if (!res)
 		return (NULL);
-	while (*s1)
+	while (s1 && *s1)
 	{
 		*res = *s1;
 		res++;
 		s1++;
 	}
-	while (*s2)
+	while (s2 && *s2)
 	{
 		*res = *s2;
 		res++;
